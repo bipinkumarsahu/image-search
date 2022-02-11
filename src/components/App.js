31b81eabc3155e35d3c6ab44f21395f56b94ec5b
Bipin Sahu <bipinsahu5@gmail.com>
2022-02-11 19:26:40 +0530

    Displaying no. of images recieved

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,10 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends react.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] };
+
+   onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: {
         query: term,
       },
@@ -13,11 +15,15 @@ class App extends react.Component {
         Authorization: "Client-ID 24Tv3q6iIi_xYvdCpq26S7Z4OXij3rslI4iZuqMFey8",
       },
     });
+    
+    this.setState({ images: response.data.results });
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+          
+        <div>Found {this.state.images.length} images</div>
       </div>
     );
   }
