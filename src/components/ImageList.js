2adce58fe6a40037cb20c62248627b794221f0b3
Bipin Sahu <bipinsahu5@gmail.com>
2022-02-13 00:53:23 +0530

    Using Ref System

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,14 @@
 import react from "react";
+import './ImageList.css'
+import ImageCard from "./ImageCard";
 
 
 const ImageList =(props)=>{
     const images = props.images.map((image)=>{
-            return <img key={image.id} className="ui large rounded centered image" alt= {image.description} src = {image.urls.regular} />
+            return <ImageCard key={image.id} image = {image} />
     })
     return(
-        <div>{images}</div>
+        <div className="image-list">{images}</div>
     )
 }
 
